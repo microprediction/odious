bbc2afae5840ea1d6ca9e713e3f30cd8692d601f
Peter Cotton <peter.cotton@microprediction.com>
2021-01-19 21:46:27 -0500

    urgh

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["odious","odious.data"],
+    packages=["odious"],
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
